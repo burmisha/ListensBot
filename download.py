08e0b51e6cf8c747493db18a4aedd011dcd86854
Mikhail Burmistrov <burmisha@gmail.com>
2018-03-09 17:28:45 +0300

    Better logging, group cmdline arguments, add more shlocberg videos, fix names

--- download.py
+++ download.py
@@ -58,7 +58,7 @@ class Track(object):
 
     def Filename(self):
         # Telegram needs mp3 extension to show mp4 files as audio
-        # Android fails on scrolling mp4 tracks
+        # Telegram on android fails on scrolling mp4 tracks
         prefix = ''
         if self.CustomPrefixDict is not None:
             for key, value in self.CustomPrefixDict.iteritems():
@@ -128,8 +128,10 @@ def downloadUrl(url, filename):
     response = requests.get(url)
     statusCode = response.status_code
     if statusCode == 200:
+        log.debug('Got code 200, writing content')
         with open(filename, 'w') as f:
             f.write(response.content)
+        log.debug('Content is ready')
     else:
         raise DownloadError('Got invalid response: {}'.format(statusCode))
 
@@ -235,6 +237,7 @@ class Mp4Track(Track):
         if self.StartShift is not None:
             command += ['-ss', self.StartShift]
         command.append(filename)
+        log.debug('Running %r', command)
         result = subprocess.call(command)
         if result != 0:
             raise 'Convert to mp3 failed'
@@ -256,17 +259,30 @@ class ShlosbergLive(object):
     def __init__(self):
         pass
 
-    def ParseTitle(self, title, date, part):
-        if u'«' in title:
-            parts = [p for p in re.split('[«»]', title) if p]
+    def FormTitle(self, goodTitle, title, date, part):
+        if goodTitle:
+            topic = goodTitle
         else:
-            parts = title.split('.', 1)
-        assert len(parts) == 2
-        topic = u'Шлосберг Live #{}, Тема: «{}» ({})'.format(part, parts[1].strip(), date.replace('-', '/'))
+            if u'«' in title:
+                parts = [p for p in re.split('[«»]', title) if p]
+            else:
+                parts = title.split('.', 1)
+            if len(parts) != 2:
+                log.warn(u'Raw title: %s', title)
+                raise RuntimeError('Broken title')
+            subTitle = u' '.join(parts[2:]).strip().strip('.')
+            if subTitle:
+                subTitle = u'. {}'.format(subTitle)
+            topic = u'Тема: «{}»{}'.format(
+                parts[1].strip(),
+                subTitle,
+            )
+        topic = u'Live #{}. {} ({})'.format(part, topic, date.replace('-', '/'))
+        topic = topic.replace('  ', ' ')
         return topic
 
     def __call__(self):
-        for url, part, shift in self.Urls():
+        for url, part, shift, customTitle in self.Urls():
             ok = False
             log.debug('Trying to fetch %r, %r, %r', url, part, shift)
             while not ok:
@@ -282,8 +298,9 @@ class ShlosbergLive(object):
             youtubeTrack = Mp4Track(audio.url, shift)
             date = video.published[0:10]
             youtubeTrack.SetEverything(
-                title=self.ParseTitle(video.title, date, part),
-                artist=video.author,
+                title=self.FormTitle(customTitle, video.title, date, part),
+                # artist=video.author,
+                artist=u'Лев Шлосберг',
                 artistEng='grazhdanin-tv',
                 playlist='shlosberg-live',
                 created=date,
@@ -296,52 +313,58 @@ class ShlosbergLive(object):
     def Urls(self):
         log.info('Videos from https://www.youtube.com/user/PskovYablokoTV/videos chosen manually')
         return [
-            ('https://www.youtube.com/watch?v=qo54lWAK1H0', '44', '0:20'),
-            ('https://www.youtube.com/watch?v=Ya20fvMFPqc', '43', '0:03'),
-            ('https://www.youtube.com/watch?v=bU-HBajBkYc', '42', '0:09'),
-            ('https://www.youtube.com/watch?v=vwyGAGulRoc', '41', '0:11'),
-            ('https://www.youtube.com/watch?v=HK6Yc5az-gA', '40', '0:11'),
-            ('https://www.youtube.com/watch?v=d_rT1_fhwBY', '39', '0:11'),
-            ('https://www.youtube.com/watch?v=okfbGIXxlQE', '38', '0:19'),
-            ('https://www.youtube.com/watch?v=HuKCihT4P64', '37', '0:26'),
-            ('https://www.youtube.com/watch?v=zKi__hj_apc', '36', '0:37'),
-            ('https://www.youtube.com/watch?v=0_h7w_JC6f4', '35', '0:09'),
-            ('https://www.youtube.com/watch?v=XmdVz34VuV4', '34', '0:05'),
-            ('https://www.youtube.com/watch?v=cUgP2C7Y7mM', '33', '0:19'),
-            ('https://www.youtube.com/watch?v=CuiADlYfjq0', '32', '1:52'),
-            ('https://www.youtube.com/watch?v=7pkAydybFCc', '31', '0:10'),
-            ('https://www.youtube.com/watch?v=ofL2yRqw9f0', '30.2', None),
-            ('https://www.youtube.com/watch?v=YVSGDJov7cw', '30.1', '0:08'),
-            ('https://www.youtube.com/watch?v=XXusqj6xygc', '29', '1:02'),
-            ('https://www.youtube.com/watch?v=QBsjBcqFev0', '28', '0:54'),
-            ('https://www.youtube.com/watch?v=YttJ60SY7sM', '27', '0:50'),
-            ('https://www.youtube.com/watch?v=JPxS1wIjUmc', '26', '1:08'),
-            ('https://www.youtube.com/watch?v=QYwTmlN0UdE', '25', '1:06'),
-            ('https://www.youtube.com/watch?v=jv_B1PXiQB8', '24', '1:08'),
-            ('https://www.youtube.com/watch?v=owBdz-X_SWQ', '23', '1:47'),
-            ('https://www.youtube.com/watch?v=HWd9l03xp1k', '22', '1:34'),
-            ('https://www.youtube.com/watch?v=ZhmZghs89wA', '21', '2:15'),
-            ('https://www.youtube.com/watch?v=72DIC22v1W4', '20', '1:16'),
-            ('https://www.youtube.com/watch?v=yXK-AZd2HAw', '19', '0:34'),
-            ('https://www.youtube.com/watch?v=fTyN66Sd9Fs', '18', '2:45'),
-            ('https://www.youtube.com/watch?v=wov7yvgTEok', '17', '1:18'),
-            ('https://www.youtube.com/watch?v=-h_KXVWVEJg', '16', '0:15'),
-            ('https://www.youtube.com/watch?v=xb0AsPuGvuc', '15', '0:49'),
-            ('https://www.youtube.com/watch?v=p2jOsznpVrk', '14', '0:08'),
-            ('https://www.youtube.com/watch?v=x5RDW-SXKA0', '13', '2:35'),
-            ('https://www.youtube.com/watch?v=pu_l_4FrRQI', '12', '1:01'),
-            ('https://www.youtube.com/watch?v=GxkhHqTKAlU', '11', '2:17'),
-            ('https://www.youtube.com/watch?v=Kf6AZOuj9dg', '10', '0:45'),
-            ('https://www.youtube.com/watch?v=23vjnCTlTjc', '9.2', '0:15'),
-            ('https://www.youtube.com/watch?v=i0-AI04ZYes', '9.1', '0:59'),
-            ('https://www.youtube.com/watch?v=jkN6Af4m9x8', '8', '0:15'),
-            ('https://www.youtube.com/watch?v=DivQCLyu_6s', '7', '0:36'),
-            ('https://www.youtube.com/watch?v=j7rL2jqhZnE', '6', '0:35'),
-            ('https://www.youtube.com/watch?v=fslL0Sjgz5U', '5', '1:36'),
-            ('https://www.youtube.com/watch?v=EP_ljk6sZvU', '4', '0:41'),
-            ('https://www.youtube.com/watch?v=jVj9L8KD3eA', '3', '0:39'),
-            ('https://www.youtube.com/watch?v=COhG3aHOs58', '2', '1:12'),
-            ('https://www.youtube.com/watch?v=X0mPF5HwaFs', '1', '36:20'),
+            ('https://www.youtube.com/watch?v=OAxFXXYIPQE', '50', '0:23',   u'World of Tanks Владимира Путина'),
+            ('https://www.youtube.com/watch?v=j_Fbry3k1kA', '49', '0:15',   u'Политический террор'),
+            ('https://www.youtube.com/watch?v=4_eeEu52_6s', '48', '0:26',   u'На Сирийском фронте без перемен'),
+            ('https://www.youtube.com/watch?v=i4HI2WCcyhA', '47', '0:25',   u'Гости: Галина Ширшина, политик и общественный деятель'),
+            ('https://www.youtube.com/watch?v=5C633UtQvuM', '46', '0:24',   u'Российская социология сегодня. Кому верить?'),
+            ('https://www.youtube.com/watch?v=nwshzE7pmRY', '45', '0:13',   u'Явлинский в Пскове и Гдове. Послесловие'),
+            ('https://www.youtube.com/watch?v=qo54lWAK1H0', '44', '0:20',   u'Гости: Григорий Явлинский'),
+            ('https://www.youtube.com/watch?v=Ya20fvMFPqc', '43', '0:03',   u'Забастовка или Явлинский'),
+            ('https://www.youtube.com/watch?v=bU-HBajBkYc', '42', '0:09',   u'Насилие в школах. Почему об этом молчит телевидение'),
+            ('https://www.youtube.com/watch?v=vwyGAGulRoc', '41', '0:11',   u'100 лет без законной власти'),
+            ('https://www.youtube.com/watch?v=HK6Yc5az-gA', '40', '0:11',   u'Назад в СССР?'),
+            ('https://www.youtube.com/watch?v=d_rT1_fhwBY', '39', '0:11',   u'2017. Политические итоги года'),
+            ('https://www.youtube.com/watch?v=okfbGIXxlQE', '38', '0:19',   u'Президент 2018. Личный выбор между добром и злом'),
+            ('https://www.youtube.com/watch?v=HuKCihT4P64', '37', '0:26',   u'Кто победил в Сирии?'),
+            ('https://www.youtube.com/watch?v=zKi__hj_apc', '36', '0:37',   u'Путин хочет еще'),
+            ('https://www.youtube.com/watch?v=0_h7w_JC6f4', '35', '0:09',   u'Допинг. Медали ценой чести и здоровья'),
+            ('https://www.youtube.com/watch?v=XmdVz34VuV4', '34', '0:05',   u'Собянин или Россия'),
+            ('https://www.youtube.com/watch?v=cUgP2C7Y7mM', '33', '0:19',   u'Правда как иностранный агент'),
+            ('https://www.youtube.com/watch?v=CuiADlYfjq0', '32', '1:52',   u'Гости: Юрий Павлов, избранный глава Гдовского района'),
+            ('https://www.youtube.com/watch?v=7pkAydybFCc', '31', '0:10',   u'1917. Переворот истории. Что делать сейчас?'),
+            ('https://www.youtube.com/watch?v=ofL2yRqw9f0', '30.2', None,   u'Политические репрессии сегодня. Вторая часть'),
+            ('https://www.youtube.com/watch?v=YVSGDJov7cw', '30.1', '0:08', u'Политические репрессии сегодня. Первая часть'),
+            ('https://www.youtube.com/watch?v=XXusqj6xygc', '29', '1:02',   u'Гости: Александр Конашенков, фермер, депутат Гдовского района'),
+            ('https://www.youtube.com/watch?v=QBsjBcqFev0', '28', '0:54',   u'Гости: Виталий Аршинов, глава Плюсского района'),
+            ('https://www.youtube.com/watch?v=YttJ60SY7sM', '27', '0:50',   u'Отставка Андрея Турчака'),
+            ('https://www.youtube.com/watch?v=JPxS1wIjUmc', '26', '1:08',   u'7/31. Свобода собраний каждый день'),
+            ('https://www.youtube.com/watch?v=QYwTmlN0UdE', '25', '1:06',   u'Как депутаты боролись с индексацией зарплат бюджетников'),
+            ('https://www.youtube.com/watch?v=jv_B1PXiQB8', '24', '1:08',   u'Матильда Российской империи'),
+            ('https://www.youtube.com/watch?v=owBdz-X_SWQ', '23', '1:47',   u'Как мы потратили ваши деньги'),
+            ('https://www.youtube.com/watch?v=HWd9l03xp1k', '22', '1:34',   u'10 сентября 2017 года. Итоги'),
+            ('https://www.youtube.com/watch?v=ZhmZghs89wA', '21', '2:15',   u'Избирательный бюллетень - главное оружие гражданина'),
+            ('https://www.youtube.com/watch?v=72DIC22v1W4', '20', '1:16',   u'Дело Кирилла Серебренникова'),
+            ('https://www.youtube.com/watch?v=yXK-AZd2HAw', '19', '0:34',   u'1991. После августа. Почему демократия не победила'),
+            ('https://www.youtube.com/watch?v=fTyN66Sd9Fs', '18', '2:45',   u'Мы ждем перемен? Обсуждение исследования ВЦИОМ'),
+            ('https://www.youtube.com/watch?v=wov7yvgTEok', '17', '1:18',   u'Рыбалка Путина'),
+            ('https://www.youtube.com/watch?v=-h_KXVWVEJg', '16', '0:15',   u'Большой Террор. Преступники и наследники. 1937-2017'),
+            ('https://www.youtube.com/watch?v=xb0AsPuGvuc', '15', '0:49',   u'Бюллетень или вилы: почему надо участвовать в выборах'),
+            ('https://www.youtube.com/watch?v=p2jOsznpVrk', '14', '0:08',   u'Дело Немцова: вопросы без ответов'),
+            ('https://www.youtube.com/watch?v=x5RDW-SXKA0', '13', '2:35',   u'Огонь по штабам'),
+            ('https://www.youtube.com/watch?v=pu_l_4FrRQI', '12', '1:01',   u'Сталин сегодня'),
+            ('https://www.youtube.com/watch?v=GxkhHqTKAlU', '11', '2:17',   u'Большой брат следит за тобой'),
+            ('https://www.youtube.com/watch?v=Kf6AZOuj9dg', '10', '0:45',   u'«Прямая линия» Путина: царь есть, государства нет'),
+            ('https://www.youtube.com/watch?v=23vjnCTlTjc', '9.2', '0:15',  u'Акции протеста в современной России. Вторая часть'),
+            ('https://www.youtube.com/watch?v=i0-AI04ZYes', '9.1', '0:59',  u'Акции протеста в современной России. Первая часть'),
+            ('https://www.youtube.com/watch?v=jkN6Af4m9x8', '8', '0:15',    u'Возвращение прямых выборов мэров в городах России'),
+            ('https://www.youtube.com/watch?v=DivQCLyu_6s', '7', '0:36',    u'Спор Шлосберга с Навальным'),
+            ('https://www.youtube.com/watch?v=j7rL2jqhZnE', '6', '0:35',    u'Прямой эфир'),
+            ('https://www.youtube.com/watch?v=fslL0Sjgz5U', '5', '1:36',    u'Прямой эфир'),
+            ('https://www.youtube.com/watch?v=EP_ljk6sZvU', '4', '0:41',    u'Прямой эфир'),
+            ('https://www.youtube.com/watch?v=jVj9L8KD3eA', '3', '0:39',    u'Прямой эфир'),
+            ('https://www.youtube.com/watch?v=COhG3aHOs58', '2', '1:12',    u'Прямой эфир'),
+            ('https://www.youtube.com/watch?v=X0mPF5HwaFs', '1', '36:20',   u'Прямой эфир'),
         ]
 
 
@@ -479,23 +502,34 @@ def main(args):
 
 def CreateArgumentsParser():
     parser = argparse.ArgumentParser('Download playlists', formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    parser.add_argument('--debug', help='Enable debug logging', action='store_true')
-    parser.add_argument('--soundcloud', help='Download soundcloud', action='store_true')
-    parser.add_argument('--shlosberg-live', help='Download Shlosberg Live', action='store_true')
-    parser.add_argument('--openuni', help='Download Open University', action='store_true')
-    parser.add_argument('--save', help='Actually save files', action='store_true')
-    parser.add_argument('--force', help='Force save even for existing files', action='store_true')
-    parser.add_argument('--secrets', help='File with custom settings', default='secrets.json')
+
+    saveGroup = parser.add_argument_group('Saving files arguments')
+    saveGroup.add_argument('--save', help='Actually save files', action='store_true')
+    saveGroup.add_argument('--force', help='Force save even for existing files', action='store_true')
+
+    podcastsGroup = parser.add_argument_group('Podcasts arguments')
+    podcastsGroup.add_argument('--soundcloud', help='Download soundcloud', action='store_true')
+    podcastsGroup.add_argument('--shlosberg-live', help='Download Shlosberg Live', action='store_true')
+    podcastsGroup.add_argument('--openuni', help='Download Open University', action='store_true')
+    podcastsGroup.add_argument('--secrets', help='File with custom settings', default='secrets.json')
+
+    loggingGroup = parser.add_argument_group('Logging arguments')
+    loggingGroup.add_argument('--log-format', help='Logging str', default='%(asctime)s %(name)15s:%(lineno)3d [%(levelname)s] %(message)s')
+    loggingGroup.add_argument('--log-separator', help='Logging string separator', choices=['space', 'tab'], default='space')
+    loggingGroup.add_argument('--verbose', help='Enable debug logging', action='store_true')
+
     return parser
 
 
 if __name__ == '__main__':
     parser = CreateArgumentsParser()
     args = parser.parse_args()
-    logging.basicConfig(
-        level=logging.DEBUG if args.debug else logging.INFO,
-        format='%(asctime)s %(name)s:%(lineno)d [%(levelname)s] %(message)s'
-    )
+
+    logFormat = args.log_format.replace('\t', ' ')
+    logFormat = logFormat.replace(' ', {'space': ' ', 'tab': '\t'}[args.log_separator])
+    logLevel = logging.DEBUG if args.verbose else logging.INFO
+    logging.basicConfig(level=logLevel, format=logFormat)
+
     log.info('Start')
     main(args)
     log.info('Finish')
